e00bf180ceb15ea84e96241ccdbc4e122b33b858
Harsh97x <Rynzie@protonmail.com>
2023-02-27 15:28:06 +0530

    fixed request failed & .env issues

--- client/script.js
+++ client/script.js
@@ -4,7 +4,7 @@ import user from "./assets/user.svg";
 const form = document.querySelector("form");
 const chatContainer = document.querySelector("#chat_container");
 
-let loadInterval = "";
+let loadInterval;
 
 function loader(element) {
   element.textContent = "";
@@ -46,46 +46,73 @@ function generateUniqueId() {
 
 function chatStripe(isAi, value, uniqueId) {
   return `
-      <div class="wrapper ${isAi && "ai"}">
-          <div class="chat">
-              <div class="profile">
-                  <img 
-                    src=${isAi ? bot : user} 
-                    alt="${isAi ? "bot" : "user"}" 
-                  />
-              </div>
-              <div class="message" id=${uniqueId}>${value}</div>
-          </div>
-      </div>
-  `;
+        <div class="wrapper ${isAi && "ai"}">
+            <div class="chat">
+                <div class="profile">
+                    <img 
+                      src=${isAi ? bot : user} 
+                      alt="${isAi ? "bot" : "user"}" 
+                    />
+                </div>
+                <div class="message" id=${uniqueId}>${value}</div>
+            </div>
+        </div>
+    `;
 }
 
-const handlesubmit = async (e) => {
+const handleSubmit = async (e) => {
   e.preventDefault();
 
-  const Data = new FormData(form);
+  const data = new FormData(form);
 
-  // user chatstripe
+  // user's chatstripe
+  chatContainer.innerHTML += chatStripe(false, data.get("prompt"));
 
-  chatContainer.innerHTML += chatStripe(false, Data.get("prompt"));
-
+  // to clear the textarea input
   form.reset();
 
-  // ai chatstripe
-
+  // bot's chatstripe
   const uniqueId = generateUniqueId();
   chatContainer.innerHTML += chatStripe(true, " ", uniqueId);
+
+  // to focus scroll to the bottom
   chatContainer.scrollTop = chatContainer.scrollHeight;
 
+  // specific message div
   const messageDiv = document.getElementById(uniqueId);
 
+  // messageDiv.innerHTML = "..."
   loader(messageDiv);
+
+  const response = await fetch("http://localhost:5000", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      prompt: data.get("prompt"),
+    }),
+  });
+
+  clearInterval(loadInterval);
+  messageDiv.innerHTML = " ";
+
+  if (response.ok) {
+    const data = await response.json();
+    const parsedData = data.bot.trim(); // trims any trailing spaces/'\n'
+
+    typeText(messageDiv, parsedData);
+  } else {
+    const err = await response.text();
+
+    messageDiv.innerHTML = "Something went wrong";
+    alert(err);
+  }
 };
 
-form.addEventListener("submit", handlesubmit);
+form.addEventListener("submit", handleSubmit);
 form.addEventListener("keyup", (e) => {
   if (e.keyCode === 13) {
-    handlesubmit(e);
+    handleSubmit(e);
   }
 });
-
